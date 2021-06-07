fae44a6934c6bdcf6d015cbb8444d3db59789782
Wisp <baezerotwo@gmail.com>
2021-06-07 11:21:32 +0200

    Yoda est vert.
    ajout de trucs cons

--- php/PDO.php
+++ php/PDO.php
@@ -1,3 +1,24 @@
 <?php
+
+try
+{
     $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+}
+
+    catch (Exception $e)
+    {
+        die('Erreur : ' .$e->getMessage());
+    }
+
+    $reponse = $bdd->query('SELECT UserName FROM Client LIMIT 0, 10');
+
+    echo '<p align="center">Vos collections :</p>';
+
+while ($donnees = $reponse->fetch())
+{
+    echo $donnees['Name'] . '<br>';
+}
+
+$reponse->closeCursor();
+    
 ?>
